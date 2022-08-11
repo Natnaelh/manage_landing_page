1f0ad193129be2c354123eca4d275a714403b59b
NatnaelH <natnaelhabtamu0@gmail.com>
2022-08-11 23:58:26 +0300

    Add NavBar, spacing and Responsive classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,24 @@ module.exports = {
   content: ["./*.html"],
   theme: {
     screens: {
-      sm: "480px",
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
       md: "768px",
-      lg: "976px",
-      xl: "1440px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
     },
     extend: {
       colors: {
